c920cab4c53eafd9d0ecb4f025b8338a439c3159
Antonio J. Delgado <ad@susurrando.com>
2022-05-07 11:54:54 +0300

    add option to install in .bashrc.d

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ansible-playbook install_bashrc_prompt_playbook.yml -l localhost -vvv
+if [ -d "${HOME}/.bashrc.d" ]; then
+    cp bashrc_ps1.sh "${HOME}/.bashrc.d/" -rfp
+else
+    ansible-playbook install_bashrc_prompt_playbook.yml -l localhost -vvv
+fi
